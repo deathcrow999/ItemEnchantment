0f783b6e2b58653f596dd6003dc113ec42ed9d6e
deathcrow999 <deathcrow4561@gmail.com>
2015-09-18 20:24:30 -0400

    Redone code from scratch

--- src/LiveWireTech/ItemEnchantment/ItemEnchantment.php
+++ src/LiveWireTech/ItemEnchantment/ItemEnchantment.php
@@ -3,56 +3,35 @@
 namespace LiveWireTech\ItemEnchantment;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\item\enchantment\EnchantmentEntry;
-use pocketmine\item\enchantment\EnchantmentList;
+use pocketmine\command\Command;
+use pocketmine\Player;
 use pocketmine\item\Item;
-use pocketmine\item\enchantment\Enchantment;
+use pocketmine\utils\Config;
+
 
 class ItemEnchantment extends PluginBase{
 
-public function onEnable(){
-$this->saveDefaultConfig();
-$this->getConfig()->set("itemid", "261");
-$this->getConfig()->set("enchantment","2");
-$this->getLogger()->info("ItemEnchantment has been loaded");
-}
+ public function onLoad(){
 
-public function onDisable(){
-$this->getLogger()->info("ItemEnchantment was disabled");
-}
-
-public function ItemHeld(PlayerItemHeldEvent $event) {
-$item = $event->getItem();
-$id = $item->getId();
-$damage = $item->getDamage();
-}
-
-public function onCommand(CommandSender $sender,Command $command,$commandLabel, array $args){
-$conItem = $this->getConfig()->get("itemid");
-$enchantment = $this->getConfig()->get("enchantment");
-
-if(strtolower ($command->getName()) === "EnchantItem"){
-  
-   if($id === $conItem){
-   $item->addEnchantment($enchantment);
-   
-
-   }
-
-}
-
-
+  $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+  $cfg->set("itemid", "261");
+  $cfg->save();
  }
 
+ public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+  $cmdName = $cmd->getName();
+  $item = new Item($sender->getInventory()->getItemInHand()->getId());
+  $conItem = $this->getConfig()->get("itemid");
 
-
-
-
-
+   if(cmdName === "enchantItem"){
+    if($item === $conItem){
+    $sender->sendMessage("Enchant Success");
+    return true;
+    }
+   }
+ $sender->sendMessage("Enchantment Failed");
+ return false;
+ }
 }
-
-
 ?>
